2e43f489f8fdc267617572518d69542aa5a64d5c
arham09 <arham.abiyan@gmail.com>
2019-02-26 17:20:41 +0700

    Add class detail

--- app/controllers/server/classes.js
+++ app/controllers/server/classes.js
@@ -35,3 +35,40 @@ exports.get = (req, res) => {
     }
   })
 }
+
+exports.getDetail = (req, res) => {
+  req.checkParams('classId', 'classId is required').notEmpty().isInt()
+
+  if (req.validationErrors()) {
+    return MiscHelper.errorCustomStatus(res, req.validationErrors(true))
+  }
+
+  const key = `get-class-${req.params.classId}`
+
+  async.waterfall([
+    (cb) => {
+      redisCache.get(key, classes => {
+        if (classes) {
+          return MiscHelper.responses(res, classes)
+        } else {
+          cb(null)
+        }
+      })
+    },
+    (cb) => {
+      classesModel.getDetail(req, req.params.classId, (errClass, resultClass) => {
+        cb(errClass, resultClass)
+      })
+    },
+    (dataClass, cb) => {
+      redisCache.setex(key, 100, dataClass)
+      cb(null, dataClass)
+    }
+  ], (errClass, resultClass) => {
+    if (!errClass) {
+      return MiscHelper.responses(res, resultClass)
+    } else {
+      return MiscHelper.errorCustomStatus(res, errClass, 400)
+    }
+  })
+}
