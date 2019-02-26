2ec1cad10af2fadf359a173202964ecd22309f63
arham09 <arham.abiyan@gmail.com>
2019-02-26 16:09:25 +0700

    Add Get class list

---
+++ app/controllers/server/classes.js
@@ -0,0 +1,37 @@
+/* global _ redisCache */
+
+'use strict'
+
+const async = require('async')
+const classesModel = require('../../models/classes')
+
+exports.get = (req, res) => {
+  const key = 'get-class'
+
+  async.waterfall([
+    (cb) => {
+      redisCache.get(key, classes => {
+        if (classes) {
+          return MiscHelper.responses(res, classes)
+        } else {
+          cb(null)
+        }
+      })
+    },
+    (cb) => {
+      classesModel.get(req, (errClass, resultClass) => {
+        cb(errClass, resultClass)
+      })
+    },
+    (dataClass, cb) => {
+      redisCache.setex(key, 100, dataClass)
+      cb(null, dataClass)
+    }
+  ], (errClass, resultClass) => {
+    if (!errClass) {
+      return MiscHelper.responses(res, resultClass)
+    } else {
+      return MiscHelper.errorCustomStatus(res, errClass, 400)
+    }
+  })
+}
