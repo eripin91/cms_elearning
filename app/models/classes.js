25ae8fc381ff8ca3fce0629dc0290d03bb300edd
arham09 <arham.abiyan@gmail.com>
2019-03-01 15:07:12 +0700

    Add Update data class & Delete data class

--- app/models/classes.js
+++ app/models/classes.js
@@ -14,7 +14,7 @@ module.exports = {
     conn.getConnection((errConnection, connection) => {
       if (errConnection) console.error(errConnection)
 
-      connection.query(`SELECT * FROM classes_tab WHERE status = 1 AND classid = ?`, classId, (err, rows) => {
+      connection.query(`SELECT a.classid, a.guruid, b.courseid, a.name AS class_name, b.name AS course_name, a.description, a.cover, a.priority, a.rating, a.created_at, b.updated_at FROM classes_tab a JOIN courses_tab b on a.classid=b.classid  WHERE a.status = 1 AND a.classid = ?`, classId, (err, rows) => {
         callback(err, rows)
       })
     })
@@ -29,6 +29,33 @@ module.exports = {
         } else {
           callback(null, _.merge(data, { classid: rows.insertId }))
         }
+      })
+    })
+  },
+  checkClass: (conn, classId, callback) => {
+    conn.getConnection((errConnection, connection) => {
+      if (errConnection) console.error(errConnection)
+
+      connection.query(`SELECT * FROM classes_tab WHERE status = 1 AND classid = ? LIMIT 1`, classId, (err, rows) => {
+        callback(err, rows)
+      })
+    })
+  },
+  updateClass: (conn, data, classId, callback) => {
+    conn.getConnection((errConnection, connection) => {
+      if (errConnection) console.error(errConnection)
+
+      connection.query(`UPDATE classes_tab SET ? WHERE classid = ?`, [data, classId], (err, rows) => {
+        callback(err, rows.affectedRows > 0 ? _.merge(data, { classid: classId }) : [])
+      })
+    })
+  },
+  deleteClass: (conn, classId, callback) => {
+    conn.getConnection((errConnection, connection) => {
+      if (errConnection) console.error(errConnection)
+
+      connection.query(`UPDATE classes_tab SET status = 0 WHERE classid = ?`, classId, (err) => {
+        callback(err, { message: 'Data has been deleted' })
       })
     })
   }
