6063789bc25eb94a6a4e4967c0ac662a99904c7e
Gratcy <gratcypalma@gmail.com>
2019-03-11 05:51:39 +0700

    Users admin (#4)
    * users get data datatables
    
    * done admin module CRUD

--- app/routes/server/v1/admin.js
+++ app/routes/server/v1/admin.js
@@ -6,8 +6,11 @@ var Route = express.Router()
 
 Route
   .all('/*', AuthHelper.requiresAuthorization)
+  .get('/get', AdminControllers.get)
   .get('/get/:adminId', AdminControllers.getDetail)
+  .post('/create', AdminControllers.create)
   .post('/update/:adminId', AdminControllers.update)
+  .get('/delete/:adminId', AdminControllers.delete)
   .post('/login', AdminControllers.login)
 
 module.exports = Route
