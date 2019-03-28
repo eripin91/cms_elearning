832897601b372e0109e40badf3b363e1bb6b26f7
Gratcy Palma <gratcypalma@gmail.com>
2019-03-28 18:21:23 +0700

    refactor discussion and course. add update discussion.

--- app/routes/server/v1/classes.js
+++ app/routes/server/v1/classes.js
@@ -6,6 +6,7 @@ var Route = express.Router()
 Route
   .all('/*', AuthHelper.requiresAuthorization)
   .get('/get', ClassesControllers.get)
+  .get('/select', ClassesControllers.getClassesSelect)
   .get('/get/:classId', ClassesControllers.getDetail)
   .get('/get-all-class', ClassesControllers.getAllClass)
   .post('/add', UploadControllers.upload.single('file'), ClassesControllers.insertClassUltimate)
