a7e2d1f7da5f51b0b9235fe6b2e59d8b26ff6529
sumirart <akun.pratamasumirat@gmail.com>
2019-03-29 15:21:57 +0700

    fix typo cannot get users by course

--- app/controllers/client/view-users.js
+++ app/controllers/client/view-users.js
@@ -39,9 +39,16 @@ exports.main = async (req, res) => {
  * @return {object} Request object
  */
 exports.stats = async (req, res) => {
-  API_SERVICE.get('v1/stats/get/' + req.params.userId, { }, (err, response) => {
+  API_SERVICE.get('v1/stats/get/' + req.params.userId, {}, (err, response) => {
     if (err) console.error(err)
-    res.render('users_stats', { data: _.result(response, 'data', { total_video_duration: 0, total_video_watch: 0, total_assessment: 0, total_module: 0 }) })
+    res.render('users_stats', {
+      data: _.result(response, 'data', {
+        total_video_duration: 0,
+        total_video_watch: 0,
+        total_assessment: 0,
+        total_module: 0
+      })
+    })
   })
 }
 
@@ -58,38 +65,57 @@ exports.ajaxGet = async (req, res) => {
   const classId = _.result(req.query, 'classid', 0)
   const ranking = _.result(req.query, 'ranking', false)
 
-  API_SERVICE.get('v1/users/get', { ranking: ranking, limit: _.result(req.query, 'length', 25), offset: _.result(req.query, 'start', 0), keyword: req.query.search['value'], classId: classId }, (err, response) => {
-    if (!err) {
-      const dataUsers = []
-      let i = 1
-      async.eachSeries(_.result(response, 'data', {}), (item, next) => {
-        item.action = MiscHelper.getActionButtonFull('users', item.userid)
-        item.action += ' <a href="users/stats/' + item.userid + '"><i class="fa fa-bar-chart-o"></i></a>'
-        item.ranking = i
-        item.score = parseInt(item.score)
-        item.confirm = MiscHelper.getConfirm(item.confirm)
-        item.created_at = moment(item.created_at).format('DD/MM/YYYY hh:mm')
-        dataUsers.push(item)
-        ++i
-        next()
-      }, err => {
-        if (!err) {
-          const data = {
-            draw: _.result(req.query, 'draw', 1),
-            recordsTotal: _.result(response, 'total', 0),
-            recordsFiltered: _.result(response, 'total', 0)
-          }
+  API_SERVICE.get(
+    'v1/users/get',
+    {
+      ranking: ranking,
+      limit: _.result(req.query, 'length', 25),
+      offset: _.result(req.query, 'start', 0),
+      keyword: req.query.search['value'],
+      classId: classId
+    },
+    (err, response) => {
+      if (!err) {
+        const dataUsers = []
+        let i = 1
+        async.eachSeries(
+          _.result(response, 'data', {}),
+          (item, next) => {
+            item.action = MiscHelper.getActionButtonFull('users', item.userid)
+            item.action +=
+              ' <a href="users/stats/' +
+              item.userid +
+              '"><i class="fa fa-bar-chart-o"></i></a>'
+            item.ranking = i
+            item.score = parseInt(item.score)
+            item.confirm = MiscHelper.getConfirm(item.confirm)
+            item.created_at = moment(item.created_at).format('DD/MM/YYYY hh:mm')
+            dataUsers.push(item)
+            ++i
+            next()
+          },
+          err => {
+            if (!err) {
+              const data = {
+                draw: _.result(req.query, 'draw', 1),
+                recordsTotal: _.result(response, 'total', 0),
+                recordsFiltered: _.result(response, 'total', 0)
+              }
 
-          return MiscHelper.responses(res, dataUsers, 200, data)
-        } else {
-          return MiscHelper.errorCustomStatus(res, err, 400)
-        }
+              return MiscHelper.responses(res, dataUsers, 200, data)
+            } else {
+              return MiscHelper.errorCustomStatus(res, err, 400)
+            }
+          }
+        )
+      } else {
+        return MiscHelper.errorCustomStatus(
+          res,
+          err,
+          _.result(err, 'status', 400)
+        )
       }
-      )
-    } else {
-      return MiscHelper.errorCustomStatus(res, err, _.result(err, 'status', 400))
     }
-  }
   )
 }
 
@@ -106,10 +132,14 @@ exports.ajaxGet = async (req, res) => {
 exports.update = async (req, res) => {
   if (_.isEmpty(req.body)) {
     const errorMsg = await MiscHelper.get_error_msg(req.sessionID)
-    API_SERVICE.get('v1/users/get/' + req.params.userId, {}, (err, response) => {
-      if (err) console.error(err)
-      res.render('users_update', { errorMsg: errorMsg, data: response.data })
-    })
+    API_SERVICE.get(
+      'v1/users/get/' + req.params.userId,
+      {},
+      (err, response) => {
+        if (err) console.error(err)
+        res.render('users_update', { errorMsg: errorMsg, data: response.data })
+      }
+    )
   } else {
     const userId = req.body.id
     const fullname = req.body.fullname
@@ -135,15 +165,22 @@ exports.update = async (req, res) => {
         )
         res.redirect('/users/update/' + userId)
       } else {
-        API_SERVICE.post('v1/users/update/' + userId, req.body, (err, response) => {
-          if (!err) {
-            MiscHelper.set_error_msg({ info: 'User berhasil diubah.' }, req.sessionID)
-            res.redirect('/users')
-          } else {
-            MiscHelper.set_error_msg({ error: err.message }, req.sessionID)
-            res.redirect('/users/update/' + userId)
+        API_SERVICE.post(
+          'v1/users/update/' + userId,
+          req.body,
+          (err, response) => {
+            if (!err) {
+              MiscHelper.set_error_msg(
+                { info: 'User berhasil diubah.' },
+                req.sessionID
+              )
+              res.redirect('/users')
+            } else {
+              MiscHelper.set_error_msg({ error: err.message }, req.sessionID)
+              res.redirect('/users/update/' + userId)
+            }
           }
-        })
+        )
       }
     }
   }
@@ -169,7 +206,10 @@ exports.delete = async (req, res) => {
       if (err) {
         MiscHelper.set_error_msg({ error: err.message }, req.sessionID)
       } else {
-        MiscHelper.set_error_msg({ info: 'User berhasil dihapus.' }, req.sessionID)
+        MiscHelper.set_error_msg(
+          { info: 'User berhasil dihapus.' },
+          req.sessionID
+        )
         res.redirect('/users')
       }
     })
